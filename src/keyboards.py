07172fb6d10fdd612ce229d6bbfc4150e98f8323
Danila Kritsky <danilakritsky@gmail.com>
2022-09-18 20:48:28 +0000

    Refactor keyboards to use ReplyKeyboardBuilder

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,30 +1,27 @@
 from aiogram import types
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+BUTTONS: list[types.KeyboardButton] = [
+    types.KeyboardButton(text="about"),
+    types.KeyboardButton(text="order"),
+    types.KeyboardButton(text="order history"),
+]
+
+PLACEHOLDER: str = "Select an action"
 
 
 def get_full_keyboard() -> types.ReplyKeyboardMarkup:
-    keyboard: list[list[types.KeyboardButton]] = [
-        [
-            types.KeyboardButton(text="about"),
-            types.KeyboardButton(text="order"),
-            types.KeyboardButton(text="order history"),
-        ]
-    ]
-    return types.ReplyKeyboardMarkup(
-        keyboard=keyboard,
-        resize_keyboard=True,
-        input_field_placeholder="Select an action",  # TODO: use constants
+    builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+    builder.row(*BUTTONS)
+    return builder.as_markup(
+        resize_keyboard=True, input_field_placeholder=PLACEHOLDER
     )
 
 
 def get_partial_keyboard() -> types.ReplyKeyboardMarkup:
-    keyboard: list[list[types.KeyboardButton]] = [
-        [
-            types.KeyboardButton(text="order"),
-            types.KeyboardButton(text="order history"),
-        ]
-    ]
-    return types.ReplyKeyboardMarkup(
-        keyboard=keyboard,
-        resize_keyboard=True,
-        input_field_placeholder="Select an action",  # TODO: use constants
+    builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+    builder.row(*BUTTONS[1:])
+    return builder.as_markup(
+        resize_keyboard=True, input_field_placeholder=PLACEHOLDER
     )
