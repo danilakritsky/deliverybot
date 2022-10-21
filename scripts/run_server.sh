e5b8921c76142b58598177247b4ac64200a8e5a9
Danila Kritsky <danilakritsky@gmail.com>
2022-10-21 20:09:47 +0000

    Change server start script to run server on all addresses

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,1 +1,3 @@
-poetry run flask --app deliverybot/server/app.py run
+poetry run flask --app deliverybot/server/app.py run --host=0.0.0.0
+# set host to 0.0.0.0 to make it available to other containers
+# https://stackoverflow.com/questions/26423984/unable-to-connect-to-flask-app-on-docker-from-host
