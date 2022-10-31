c90b9f3ac6cc4dbdf26881f884f0f7e781db4d60
Danila Kritsky <danilakritsky@gmail.com>
2022-10-31 23:15:33 +0000

    Update images

--- deliverybot/server/app.py
+++ deliverybot/server/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, send_from_directory
-
+from flask_cors import CORS
 
 app = Flask(__name__, static_url_path="/static/photos")
+CORS(app)
 
 
 # https://naysan.ca/2021/02/28/flask-101-serve-images/
 @app.route("/photos/<photo_name>")
 def send_photo(photo_name):
     return send_from_directory("./static/photos", f"{photo_name}")
+
 
 @app.route("/")
 def index():
--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,3 +1,8 @@
-poetry run flask --app deliverybot/server/app.py run --host=0.0.0.0
+# poetry run flask --app deliverybot/server/app.py run --host=0.0.0.0
 # set host to 0.0.0.0 to make it available to other containers
 # https://stackoverflow.com/questions/26423984/unable-to-connect-to-flask-app-on-docker-from-host
+
+
+# GUNICORN
+# https://flask.palletsprojects.com/en/2.2.x/deploying/gunicorn/#binding-externally
+poetry run gunicorn --bind 0.0.0.0:5000 deliverybot.server.app:app
